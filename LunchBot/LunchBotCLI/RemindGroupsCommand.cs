73c8c4bef9d3f56b022c949586c99e6765a64c15
Callum Rose <callum.rose@flightclubdarts.com>
2022-10-12 16:08:08 +0100

    The "remind" command now displays the names of users in silent groups

--- LunchBot/LunchBotCLI/RemindGroupsCommand.cs
+++ LunchBot/LunchBotCLI/RemindGroupsCommand.cs
@@ -1,4 +1,5 @@
-﻿using LunchBot;
+﻿using System.Text;
+using LunchBot;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Graph;
 using Serilog;
@@ -24,9 +25,11 @@ internal class RemindGroupsCommand : CommandBase
     private readonly ILogger _logger;
     private readonly MessageAuthor _messageAuthor;
     private readonly ChatHandler _chatHandler;
+    private readonly AppDataFiler _appDataFiler;
 
     public RemindGroupsCommand(GraphServiceClient graphServiceClient, LunchDataFiler lunchDataFiler,
-        LunchDataHelper lunchDataHelper, ILogger logger, MessageAuthor messageAuthor, ChatHandler chatHandler)
+        LunchDataHelper lunchDataHelper, ILogger logger, MessageAuthor messageAuthor, ChatHandler chatHandler, 
+        AppDataFiler appDataFiler)
     {
         _graphServiceClient = graphServiceClient;
         _lunchDataFiler = lunchDataFiler;
@@ -34,10 +37,22 @@ internal class RemindGroupsCommand : CommandBase
         _logger = logger;
         _messageAuthor = messageAuthor;
         _chatHandler = chatHandler;
+        _appDataFiler = appDataFiler;
     }
 
     protected override async Task<int> OnExecute(CommandLineApplication app)
     {
+        if (IsDryRun)
+        {
+            Console.WriteLine("Doing dry run");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("NOT A TEST, THIS IS THE REAL SHIT!!");
+            Console.ResetColor();
+        }
+        
         if (string.IsNullOrWhiteSpace(LunchDataPath))
         {
             if (!_lunchDataHelper.TryPromptForLunchData(out string lunchDataPath))
@@ -49,19 +64,21 @@ internal class RemindGroupsCommand : CommandBase
             LunchDataPath = lunchDataPath;
         }
 
-        if (IsDryRun)
-        {
-            Console.WriteLine("Doing dry run");
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("NOT A TEST, THIS IS THE REAL SHIT!!");
-            Console.ResetColor();
-        }
-
         LunchData lunchData = await _lunchDataFiler.Load(LunchDataPath);
 
+        User conductor = await _graphServiceClient.Me.Request().GetAsync();
+
+        try
+        {
+            AppData appData = await _appDataFiler.Load();
+            Assert.ConductorIsExpected(conductor, appData.ConductorDisplayName);
+        }
+        catch (Exception e)
+        {
+            _logger.Information(e, "Exiting");
+            return await CommandHelper.ExecuteRootCommand(app);
+        }
+        
         if (lunchData.WasDryRun)
         {
             Console.WriteLine("FYI this lunch was a dry run");
@@ -71,35 +88,68 @@ internal class RemindGroupsCommand : CommandBase
         {
             Console.WriteLine("FYI this lunch was not successful");
         }
-        
+
+        IReadOnlyList<string> silentChatIds = await FindSilentChats(lunchData, conductor);
+
+        if (!silentChatIds.Any())
+        {
+            _logger.Information("There are no silent chats");
+            return await CommandHelper.ExecuteRootCommand(app);
+        }
+
+        await DisplaySilentChats(silentChatIds, conductor.DisplayName);
+
         if (!Blocker.RequestUserCodeVerification("Enter code to send reminders"))
         {
             _logger.Warning("Incorrect code entered. Cancelling reminder");
             return await CommandHelper.ExecuteRootCommand(app);
         }
 
-        await SendReminders(lunchData);
+        await SendReminders(silentChatIds);
 
         _logger.Information("Finished sending reminder messages");
 
         return await CommandHelper.ExecuteRootCommand(app);
     }
 
-    private async Task SendReminders(LunchData lunchData)
+    private async Task DisplaySilentChats(IReadOnlyList<string> silentChatIds, string conductorDisplayName)
     {
-        User conductor = await _graphServiceClient.Me.Request().GetAsync();
+        StringBuilder stringBuilder = new("Chats with these members haven't messaged yet:\n");
 
-        int failedCount = 0;
-
-        foreach (string chatId in lunchData!.Chats.Select(c => c.ChatId))
+        for (int i = 0; i < silentChatIds.Count; i++)
         {
-            bool chatUsersHaveMessaged = await ChatUsersHaveMessaged(chatId, conductor);
-
-            if (chatUsersHaveMessaged)
+            string chatId = silentChatIds[i];
+            
+            IChatMembersCollectionPage? members;
+            
+            try
             {
+                members = await _graphServiceClient.Chats[chatId].Members.Request().GetAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, $"Could not find chat members: {chatId}");
                 continue;
             }
 
+            IEnumerable<string> usersNames = members.CurrentPage
+                .Where(m => m.DisplayName != conductorDisplayName)
+                .Select(m => m.DisplayName.Split(" ").First())
+                .OrderBy(n => n);
+            string membersStr = string.Join(", ", usersNames);
+            
+            stringBuilder.AppendLine($"\t{i}: {membersStr}");
+        }
+        
+        Console.WriteLine(stringBuilder);
+    }
+
+    private async Task SendReminders(IEnumerable<string> chatIds)
+    {
+        int failedCount = 0;
+
+        foreach (string chatId in chatIds)
+        {
             bool success = await SendReminder(chatId);
 
             if (!success)
@@ -112,6 +162,23 @@ internal class RemindGroupsCommand : CommandBase
         {
             _logger.Error($"Failed to send reminder to {failedCount} chats");
         }
+    }
+    
+    private async Task<IReadOnlyList<string>> FindSilentChats(LunchData lunchData, User conductor)
+    {
+        List<string> silentChatIds = new();
+        
+        foreach (string chatId in lunchData.Chats.Select(c => c.ChatId))
+        {
+            bool chatUsersHaveMessaged = await ChatUsersHaveMessaged(chatId, conductor);
+
+            if (!chatUsersHaveMessaged)
+            {
+                silentChatIds.Add(chatId);
+            }
+        }
+
+        return silentChatIds;
     }
 
     private async Task<bool> SendReminder(string chatId)
@@ -132,7 +199,7 @@ internal class RemindGroupsCommand : CommandBase
         }
         catch (Exception e)
         {
-            _logger.Error(e, $"Could not find chat: {chatId}");
+            _logger.Error(e, $"Could not find chat messages: {chatId}");
             return false;
         }
 
